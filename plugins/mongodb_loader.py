09fa1f889a74f68b60cd80cee00a81ff51c8de81
zidanz1112 <zidanalizaqi51@gmail.com>
2024-07-03 11:01:43 +0700

    Upload Files

---
+++ plugins/mongodb_loader.py
@@ -0,0 +1,44 @@
+import pymongo
+import config as conf
+
+def get_mongo_client(mongo_uri):
+    """Establish connection to the MongoDB."""
+    try:
+        client = pymongo.MongoClient(mongo_uri)
+        # Send a ping to confirm a successful connection
+        try:
+            client.admin.command('ping')
+            print("Pinged your deployment. You successfully connected to MongoDB!")
+
+            return client
+        except Exception as e:
+            print(e)
+    except pymongo.errors.ConnectionFailure as e:
+        print(f"Connection failed: {e}")
+        return None
+
+def load(database, collection):
+    mongo_uri = conf.mongo_uri
+    if not mongo_uri:
+        print("MONGO_URI not set in environment variables")
+
+    mongo_client = get_mongo_client(mongo_uri)
+
+    # Ingest data into MongoDB
+    db = mongo_client[database]
+    col = db[collection]
+
+    return col
+
+def get_data(database, collection):
+    mongo_uri = conf.mongo_uri
+    if not mongo_uri:
+        print("MONGO_URI not set in environment variables")
+
+    mongo_client = get_mongo_client(mongo_uri)
+
+    # Ingest data into MongoDB
+    db = mongo_client[database]
+    col = db[collection]
+
+    return db
